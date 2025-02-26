32dcffde0a13bfc8077f379c895a6969355b649e
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-02-25 22:43:49 -0300

    Atualizando rotas
    - Adicionando as rotas referentes a atualização de funcionário

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::get('/', [UserController::class, 'index'])->name('user.index');
 Route::get('/create-funcionario', [UserController::class, 'create'])->name('user.create');
 Route::post('/store-funcionario', [UserController::class, 'store'])->name('user.store');
 Route::get('/show-funcionario/{user}', [UserController::class, 'show'])->name('user.show');
+Route::get('/edit-funcionario/{user}', [UserController::class, 'edit'])->name('user.edit');
+Route::put('/update-funcionario/{user}', [UserController::class, 'update'])->name('user.update');
