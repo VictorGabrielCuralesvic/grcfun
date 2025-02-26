d453a3afd928953f63a3d94348dcbfa2127a5654
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-02-25 21:34:51 -0300

    Criado rota de visualizar funcionário
    - Criado a rota para visualizar somente um funcionário

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [UserController::class, 'index'])->name('user.index');
 Route::get('/create-funcionario', [UserController::class, 'create'])->name('user.create');
 Route::post('/store-funcionario', [UserController::class, 'store'])->name('user.store');
+Route::get('/show-funcionario/{user}', [UserController::class, 'show'])->name('user.show');
