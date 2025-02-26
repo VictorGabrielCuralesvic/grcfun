806b2163ddc9f81743fd06bac79b1ca20afb19f4
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-02-26 10:31:21 -0300

    Atualizado o index.blade
    - O arquivo index.blade foi atualizado com estilização do bootstrap para ficar mais apresentável.
    - A interface agora apresenta um card bem montado, além do layout e botões intuitivos.

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -1,44 +1,66 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Curalesvic</title>
-</head>
-<body>
+@extends('layouts.admin')
 
-    <a href="{{ route('user.create')}}">Cadastrar</a><br>
-    <h2>Listar Funcionários</h2>
+@section('content')
+    <div class="card mt-4 mb-4 border-light shadow">
+        <div class="card-header hstack gap-2">
+            <span>Listar Funcionários</span>
+            <span class="ms-auto">
+                <a href="{{ route('user.create') }}" class="btn btn-success btn-sm">Cadastrar</a>
+            </span>
+        </div>
 
-    @if (session('success'))
-        <p style="color: green">
-            {{ session('success') }}
-        </p>    
-    @endif
+        <div class="card-body">
+            
+            <x-alert />
 
-    @forelse ($users as $user)
-        ID: {{ $user->id}} <br>
-        Nome: {{ $user->name }} <br>
-        Email: {{ $user->email }} <br>
-        CPF: {{ $user->cpf }} <br>
-        Data de Nascimento: {{ $user->data_nascimento }} <br>
-        Telefone: {{ $user->telefone }} <br>
-        Gênero: {{ $user->genero }} <br>
-
-        <a href="{{ route('user.show', ['user' => $user->id])}}">Visualizar</a> <br>
-        <a href="{{ route('user.edit', ['user' => $user->id])}}">Editar</a> <br>
-        <a href="{{ route('user.destroy', ['user' => $user->id])}}">Deletar</a> <br>
-
-        <form method="POST" action="{{ route('user.destroy', ['user' => $user->id] )}}">
-            @csrf
-            @method('DELETE')
-            <button type="submit" onclick="return confirm('Tem certeza que deseja deletar este funcionário?')">Apagar</button>
-        </form>
-
-        <hr>
-    @empty
-        <p>Nenhum usuário cadastrado.</p>
-    @endforelse
-    
-</body>
-</html>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col" class="text-center">ID</th>
+                        <th scope="col" class="text-center">Nome</th>
+                        <th scope="col" class="text-center">E-mail</th>
+                        <th scope="col" class="text-center">CPF</th>
+                        <th scope="col" class="text-center">Data de Nascimento</th>
+                        <th scope="col" class="text-center">Telefone</th>
+                        <th scope="col" class="text-center">Gênero</th>
+                        <th scope="col" class="text-center">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($users as $user)
+                        <tr>
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>{{ $user->cpf }}</td>
+                            <td>{{ $user->data_nascimento }}</td>
+                            <td>{{ $user->telefone }}</td>
+                            <td>{{ $user->genero }}</td>
+                            <td class="text-center">
+                                <div class="d-flex gap-2">
+                                    <a href="{{ route('user.show', ['user' => $user->id]) }}" class="btn btn-primary btn-sm">
+                                        Visualizar
+                                    </a>
+                                    <a href="{{ route('user.edit', ['user' => $user->id]) }}" class="btn btn-warning btn-sm">
+                                        Editar
+                                    </a>
+                                    <form method="POST" action="{{ route('user.destroy', ['user' => $user->id]) }}">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" onclick="return confirm('Tem certeza que deseja deletar este funcionário?')" class="btn btn-danger btn-sm">
+                                            Apagar
+                                        </button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="8" class="text-center">Nenhum usuário cadastrado.</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
