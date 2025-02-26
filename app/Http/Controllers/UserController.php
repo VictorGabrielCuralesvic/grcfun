99ce369ca500e6a6692f3f825645262ebb8dd097
VictorGabrielCuralesvic <miranhaomi@gmail.com>
2025-02-25 21:35:24 -0300

    Implementado lógica de visualizar o funcionário

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
+
+        // listar funcionarios
+        $users = User::orderByDesc('id')->get();
         // carregar view
-        return view('users.index');
+        return view('users.index', ['users' => $users]);
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', ['user' => $user]);
     }
 
     public function create()
@@ -18,8 +28,23 @@ class UserController extends Controller
         return view('users.create');
     }
 
-    public function store()
+    public function store(UserRequest $request)
     {
-        
+        // validar o formulário
+        $request->validated();
+
+        // cadastrar funcionario
+        User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+            'cpf' => $request->cpf,
+            'data_nascimento' => $request->data_nascimento,
+            'telefone' => $request->telefone,
+            'genero' => $request->genero,
+        ]);
+
+        // redirecionar usuário
+        return redirect()->route('user.index')->with('success', 'Usuário cadastrado com sucesso');
     }
 }
